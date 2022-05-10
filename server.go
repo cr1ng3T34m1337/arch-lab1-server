e60185a22f4b2dca38db7ea6c427ee99c1066612
kcholombytko <funnygungan@gmail.com>
2022-05-10 12:26:07 +0300

    implement main function

--- server.go
+++ server.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"http",
+	"log"
+)
 
+func main() {
+	http.HandleFunc("/time", getTime)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
